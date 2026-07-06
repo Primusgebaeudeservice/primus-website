a6817f35f24babf18b946145aea2c4d040976439
yunuto65 <yunus65@hotmail.de>
2026-07-06 04:19:32 +0200

    feat: integrate legally audited T&C (AGB), revocation policies, sitemap update, and complete physical address details

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: 'index.html',
         impressum: 'impressum.html',
+        agb: 'agb.html',
         datenschutz: 'datenschutz.html'
       }
     }
